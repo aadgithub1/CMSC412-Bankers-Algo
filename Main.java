1f1ae35e1532ec025d3ea8eff81c9d96dcbf8142
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-01-24 11:03:50 -0500

    Read number of p and r.

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            File file = new File("input.txt");
+            Scanner scanner = new Scanner(file);
+            int[] items = new int[2];
+            for(int i = 0; i < 2; i++){
+                items[i] = scanner.nextInt();
+            }
+            for(int item : items){
+                System.out.println(item);
+            }
+            scanner.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
