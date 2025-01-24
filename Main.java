aa75de401fd99062b80b394948fca40aee40f854
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-01-24 17:07:29 -0500

    Fix functionality.

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main{
+    static int nProcesses;
+    static int mResourceTypes;
     public static void main(String[] args) {
         try{
             File file = new File("input.txt");
             Scanner scanner = new Scanner(file);
-            int nProcesses = scanner.nextInt();
-            int mResourceTypes = scanner.nextInt();
+            nProcesses = scanner.nextInt();
+            mResourceTypes = scanner.nextInt();
             
             int[] systemResources = new int[mResourceTypes];
             for(int i = 0; i < mResourceTypes; i++){
@@ -27,6 +30,11 @@ public class Main{
                 }
             }
 
+            // for(int item : available){
+            //     System.out.print(item);
+            // }
+            // System.out.println();
+
             int[][] claim = new int[nProcesses][mResourceTypes];
             for(int i = 0; i < nProcesses; i++){
                 for(int j = 0; j < mResourceTypes; j++){
@@ -40,8 +48,31 @@ public class Main{
                     need[i][j] = claim[i][j] - allocation[i][j];
                 }
             }
-            
+
+            ArrayList<Integer> seq = new ArrayList<>();
+            while(seq.size() < nProcesses){
+                for(int i = 0; i < nProcesses; i++){
+                    for(int j = 0; j < mResourceTypes; j++){
+                        if(claim[i][j] <= available[j] + allocation[i][j]){
+                            if(j == mResourceTypes - 1 && !seq.contains(i)){
+                                seq.add(i);
+                                for(int q = 0; q < mResourceTypes; q++){
+                                    available[q] += allocation[i][q];
+                                    // System.out.print(available[q]);
+                                }
+                                // System.out.println();
+                            }
+                            continue;
+                        } else{
+                            break;
+                        }
+                    }
+                }
+            }
             scanner.close();
+            for(Integer process : seq){
+                System.err.println("process " + (process+1));
+            }
         } catch(Exception e){
             e.printStackTrace();
         }
