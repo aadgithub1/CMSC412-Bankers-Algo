af55a7acb77aa3669eb239a78682b52eaefbaba8
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-01-25 08:17:11 -0500

    Works again.

--- Arena.java
+++ Arena.java
@@ -1,9 +1,9 @@
 public class Arena {
     public static void main(String[] args) {
         int m = 2;
-        int[] array = new int[m];
-        for(int i = 0; i < m; i++){
-            System.out.println(array[i]);
+        boolean[] fin = new boolean[6];
+        for(boolean t : fin){
+            System.out.println(t);
         }
     }
     
--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main{
@@ -8,79 +7,71 @@ public class Main{
     static int mResourceTypes;
     public static void main(String[] args) {
         try{
-            for(int m = 0; m < 2; m++){
-                File file = new File("input.txt");
-                Scanner scanner = new Scanner(file);
-                nProcesses = scanner.nextInt();
-                mResourceTypes = scanner.nextInt();
-                Random random = new Random();
-                int randint = random.nextInt(nProcesses);
-                
-                int[] systemResources = new int[mResourceTypes];
-                for(int i = 0; i < mResourceTypes; i++){
-                    systemResources[i] = scanner.nextInt();
-                }
+            File file = new File("input.txt");
+            Scanner scanner = new Scanner(file);
+            nProcesses = scanner.nextInt();
+            mResourceTypes = scanner.nextInt();
+            
+            int[] systemResources = new int[mResourceTypes];
+            for(int i = 0; i < mResourceTypes; i++){
+                systemResources[i] = scanner.nextInt();
+            }
 
-                int[] available = new int[mResourceTypes];
-                int[][] allocation = new int[nProcesses][mResourceTypes];
-                for(int i = 0; i < nProcesses; i++){
-                    for(int j = 0; j < mResourceTypes; j++){
-                        allocation[i][j] = scanner.nextInt();
-                        if(i == 0){
-                            available[j] = systemResources[j] - allocation[i][j];
-                        } else{
-                            available[j] -= allocation[i][j];
-                        }
+            int[] available = new int[mResourceTypes];
+            int[][] allocation = new int[nProcesses][mResourceTypes];
+            for(int i = 0; i < nProcesses; i++){
+                for(int j = 0; j < mResourceTypes; j++){
+                    allocation[i][j] = scanner.nextInt();
+                    if(i == 0){
+                        available[j] = systemResources[j] - allocation[i][j];
+                    } else{
+                        available[j] -= allocation[i][j];
                     }
                 }
+            }
 
-                // for(int item : available){
-                //     System.out.print(item);
-                // }
-                // System.out.println();
+            //TO-DO
+            //Make it find exactly 2 sequences
+            //Make UI
+            int[][] claim = new int[nProcesses][mResourceTypes];
+            for(int i = 0; i < nProcesses; i++){
+                for(int j = 0; j < mResourceTypes; j++){
+                    claim[i][j] = scanner.nextInt();
+                }
+            }
 
+            int[][] need = new int[nProcesses][mResourceTypes];
+            for(int i = 0; i < nProcesses; i++){
+                for(int j = 0; j < mResourceTypes; j++){
+                    need[i][j] = claim[i][j] - allocation[i][j];
+                }
+            }
 
-                //TO-DO
-                //Make it find exactly 2 sequences
-                //Make UI
-                int[][] claim = new int[nProcesses][mResourceTypes];
+            boolean[] finished = new boolean[nProcesses];
+            ArrayList<Integer> seq = new ArrayList<>();
+            while(seq.size() < nProcesses){
                 for(int i = 0; i < nProcesses; i++){
                     for(int j = 0; j < mResourceTypes; j++){
-                        claim[i][j] = scanner.nextInt();
-                    }
-                }
-
-                int[][] need = new int[nProcesses][mResourceTypes];
-                for(int i = 0; i < nProcesses; i++){
-                    for(int j = 0; j < mResourceTypes; j++){
-                        need[i][j] = claim[i][j] - allocation[i][j];
-                    }
-                }
-
-                ArrayList<Integer> seq = new ArrayList<>();
-                while(seq.size() < nProcesses){
-                    for(int i = randint; i < nProcesses; i++){
-                        for(int j = 0; j < mResourceTypes; j++){
-                            if(claim[i][j] <= available[j] + allocation[i][j]){
-                                if(j == mResourceTypes - 1 && !seq.contains(i)){
-                                    seq.add(i);
-                                    for(int q = 0; q < mResourceTypes; q++){
-                                        available[q] += allocation[i][q];
-                                        // System.out.print(available[q]);
-                                    }
-                                    // System.out.println();
+                        if(claim[i][j] <= available[j] + allocation[i][j]){
+                            if(j == mResourceTypes - 1 && finished[i] == false){
+                                seq.add(i);
+                                finished[i] = true;
+                                for(int q = 0; q < mResourceTypes; q++){
+                                    available[q] += allocation[i][q];
+                                    // System.out.print(available[q]);
                                 }
-                                continue;
-                            } else{
-                                break;
+                                // System.out.println();
                             }
+                            continue;
+                        } else{
+                            break;
                         }
                     }
                 }
-                scanner.close();
-                for(Integer process : seq){
-                    System.err.println("process " + (process+1));
-                }
+            }
+            scanner.close();
+            for(Integer process : seq){
+                System.err.println("process " + (process+1));
             }
         } catch(Exception e){
             e.printStackTrace();
