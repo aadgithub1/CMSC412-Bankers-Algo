37f63af9fafdc61595fac710b0ed352a15b4a94f
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-01-27 09:00:44 -0500

    Back to one sequence logic.

--- Main.java
+++ Main.java
@@ -52,83 +52,37 @@ public class Main{
                 }
             }
 
-            // boolean[] finished = new boolean[nProcesses];
-            ArrayList<Integer> tempSeq = new ArrayList<>();
             ArrayList<Integer> seq = new ArrayList<>();
-            ArrayList<Integer> seq2 = new ArrayList<>();
-            int[] work = available.clone();
             int zeroCounter = 0;
-            boolean foundTwoSeqs = false;
-            while(seq.size() < nProcesses || seq2.size() < nProcesses){
+            while(seq.size() < nProcesses){
                 for(int i = 0; i < nProcesses; i++){
                     for(int j = 0; j < mResourceTypes; j++){
                         if(i == 0){
                             zeroCounter++;
                         }
-                        if(claim[i][j] <= work[j] + allocation[i][j]){
-                            if(j == mResourceTypes - 1 && !tempSeq.contains(i)){
-                                tempSeq.add(i);
-                                
+                        if(claim[i][j] <= available[j] + allocation[i][j]){
+                            if(j == mResourceTypes - 1 && !seq.contains(i)){
+                                seq.add(i);
                                 for(int q = 0; q < mResourceTypes; q++){
-                                    work[q] += allocation[i][q];
-                                    System.out.print(work[q]);
-                                    
+                                    available[q] += allocation[i][q];
+                                    // System.out.print(available[q]);
                                 }
-                                System.out.println();
                             }
                         } else{
                             break;
                         }
-                        System.out.println("temp " + tempSeq);
-                        System.out.println("seq " + seq);
-                        System.out.println("seq2 " + seq2);
-                    }
-                    // System.out.println("temp: " + tempSeq.size());
-                }
-                
-                if(tempSeq.size() == nProcesses){
-                    if(seq.isEmpty()){
-                        seq = new ArrayList<>(tempSeq);
-                        // for(int thing : seq){
-                        //     System.err.println("thing: " + thing);
-                        // }
-                        tempSeq.clear();
-                        work = available.clone();
-                        zeroCounter = 0;
-                    } else if(!seq.isEmpty()){
-                        seq2 = new ArrayList<>(tempSeq);
-                        tempSeq.clear();
-                        foundTwoSeqs = true;
                     }
                 }
-                if(foundTwoSeqs){
-                    break;
-                } else if(zeroCounter > 4){
+                if(zeroCounter > 4){
                     System.out.println("infinite");
                     break;
                 }
-                // System.out.println(zeroCounter);
-
+            }
+            System.out.println("the sequence is: ");
+            for(int i = 0; i < seq.size(); i++){
+                System.out.println((seq.get(i)+1) + ", ");
             }
             scanner.close();
-            if(!seq.isEmpty() && !seq2.isEmpty()){
-                for(Integer process : seq){
-                    System.err.println("process " + (process+1));
-                }
-                for(Integer process : seq2){
-                    System.err.println("process2 " + (process+1));
-                }
-            } else if(!seq.isEmpty() && seq2.isEmpty()){
-                for(Integer process : seq){
-                    System.err.println("process " + (process+1));
-                }
-            } else if(seq.isEmpty() && !seq2.isEmpty()){
-                for(Integer process : seq2){
-                    System.err.println("process " + (process+1));
-                }
-            } else{
-                System.out.println("there are no safe sequences!");
-            }
         } catch(Exception e){
             e.printStackTrace();
         }
