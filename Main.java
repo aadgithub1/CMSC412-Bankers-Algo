bd6e68113b7f1acbebccb80ced06b634a9221112
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-01-27 06:06:01 -0500

    Implement incorrect search.

--- Main.java
+++ Main.java
@@ -57,14 +57,18 @@ public class Main{
             ArrayList<Integer> seq = new ArrayList<>();
             ArrayList<Integer> seq2 = new ArrayList<>();
             int[] work = available.clone();
+            int zeroCounter = 0;
             boolean foundTwoSeqs = false;
             while(seq.size() < nProcesses || seq2.size() < nProcesses){
                 for(int i = 0; i < nProcesses; i++){
                     for(int j = 0; j < mResourceTypes; j++){
+                        if(i == 0){
+                            zeroCounter++;
+                        }
                         if(claim[i][j] <= work[j] + allocation[i][j]){
                             if(j == mResourceTypes - 1 && !tempSeq.contains(i)){
                                 tempSeq.add(i);
-                                // finished[i] = true;
+                                
                                 for(int q = 0; q < mResourceTypes; q++){
                                     work[q] += allocation[i][q];
                                     System.out.print(work[q]);
@@ -90,6 +94,7 @@ public class Main{
                         // }
                         tempSeq.clear();
                         work = available.clone();
+                        zeroCounter = 0;
                     } else if(!seq.isEmpty()){
                         seq2 = new ArrayList<>(tempSeq);
                         tempSeq.clear();
@@ -98,14 +103,31 @@ public class Main{
                 }
                 if(foundTwoSeqs){
                     break;
+                } else if(zeroCounter > 4){
+                    System.out.println("infinite");
+                    break;
                 }
+                // System.out.println(zeroCounter);
+
             }
             scanner.close();
-            for(Integer process : seq){
-                System.err.println("process " + (process+1));
-            }
-            for(Integer process : seq2){
-                System.err.println("process2 " + (process+1));
+            if(!seq.isEmpty() && !seq2.isEmpty()){
+                for(Integer process : seq){
+                    System.err.println("process " + (process+1));
+                }
+                for(Integer process : seq2){
+                    System.err.println("process2 " + (process+1));
+                }
+            } else if(!seq.isEmpty() && seq2.isEmpty()){
+                for(Integer process : seq){
+                    System.err.println("process " + (process+1));
+                }
+            } else if(seq.isEmpty() && !seq2.isEmpty()){
+                for(Integer process : seq2){
+                    System.err.println("process " + (process+1));
+                }
+            } else{
+                System.out.println("there are no safe sequences!");
             }
         } catch(Exception e){
             e.printStackTrace();
