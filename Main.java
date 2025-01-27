15dafba83762804298d313535ebe555ecb39f3b6
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-01-27 11:25:01 -0500

    implement single seq.

--- Main.java
+++ Main.java
@@ -44,51 +44,22 @@ public class Main{
                 }
             }
 
-            
-            ArrayList<Integer> validStartPoints = new ArrayList<>();
-            //create a list of potential safe starting points
-                //if need for this process and this resource < available + allocation
-                    //add to list
-
-                //for each item in the list
-                    //print item i is a valid starting point
-
-
-            for(int k = 0; k < nProcesses; k++){
-                for(int l = 0; l < mResourceTypes; l++){
-                    if(need[k][l] <= allocation[k][l]){
-                        if(l == mResourceTypes - 1 && !validStartPoints.contains(k)){
-                            validStartPoints.add(k);
-                            
-                        }
-                    }
-                }
-            }
-
             ArrayList<Integer> seq = new ArrayList<>();
-            int zeroCounter = 0;
             while(seq.size() < nProcesses){
                 for(int i = 0; i < nProcesses; i++){
                     for(int j = 0; j < mResourceTypes; j++){
-                        if(i == 0){
-                            zeroCounter++;
-                        }
                         if(need[i][j] <= available[j]){
                             if(j == mResourceTypes - 1 && !seq.contains(i)){
                                 seq.add(i);
                                 for(int q = 0; q < mResourceTypes; q++){
                                     available[q] += allocation[i][q];
-                                    // System.out.print(available[q]);
+                                    System.out.println(available[q]);
                                 }
                             }
                         } else{
                             break;
                         }
                     }
-                }
-                if(zeroCounter > 10){
-                    System.out.println("infinite");
-                    break;
                 }
             }
             scanner.close();
